03682fade5157329099d929e032335892cb7e6c7
Simonebaca <simobaco99@gmail.com>
2023-03-07 19:17:07 +0100

    fix-passwordfinale

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 
+//uso prompt per chiedere il nome
+let userName = prompt("Please enter your name");
+console.log(userName);
 
-// uso prompt per chiedere il nome all'utente
-    let username = prompt("Please enter your name", "Simone");
-// uso prompt per chiedere il cognome all'utente
-    let usersurname = prompt("Please enter your surname", "Bacaloni");
-// uso prompt per chiedere il colore preferito all'utente
-    let usercolor = prompt("Please enter your favourite color", "Verde");
+//uso prompt per chiedere il cognome
+
+userSurname = prompt("Please enter your surname");
+console.log(userSurname);
+
+//uso prompt let per chiedere il colore
+
+usercolor = prompt("Please enter your favourite color");
+console.log(usercolor);
 
    
-// uso var let  per il formato nomecognomecolore23 da scrivere sulla pagina
-let nome = "Simone";
-let surname = "Bacaloni";
-let  color = "Verde";
-let number = "23" ;
-let format = nome + surname + color + number;
-document.write(format);
+//  footer :uso var let  per il formato nomecognomecolore23 
+let result = userName + userSurname + usercolor + 23;
+console.log(result);
+let element = document.getElementById("result_display");
+console.log ();
+element.innerHTML = result;
