3a1f8e09fe27b9065ebc3f770e4455f7d1796812
Simonebaca <simobaco99@gmail.com>
2023-03-07 16:13:18 +0100

    beta-main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+// uso prompt per chiedere il nome all'utente
+    let username = prompt("Please enter your name", "Simone");
+// uso prompt per chiedere il cognome all'utente
+    let usersurname = prompt("Please enter your surname", "Bacaloni");
+// uso prompt per chiedere il colore preferito all'utente
+    let usercolor = prompt("Please enter your favourite color", "Verde");
+
+   
+// uso var per il formato nomecognomecolore23 da scrivere sulla pagina
+let nome = "Simone";
+let surname = "Bacaloni";
+let  color = "Verde";
+let number = "23" ;
+let format = nome + surname + color + number;
+document.write(format);
